8cc961dfa3419e7dab417428121bfaf78c2f58d1
Marko Bošković <bosskovic@gmail.com>
2019-10-07 10:38:27 +0200

    implements the missing update action for ZoneTranslations

--- app/controllers/zone_translations_controller.rb
+++ app/controllers/zone_translations_controller.rb
@@ -1,2 +1,13 @@
 class ZoneTranslationsController < ApplicationController
+
+  def update
+    @zone_translation = ZoneTranslation.find params['id']
+    @zone_translation.update zone_translation_params
+  end
+
+  private
+
+  def zone_translation_params
+    params.require(:zone_translation).permit([:name, :description, :price, :price_table])
+  end
 end
